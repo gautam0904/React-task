6b1dcbe2ef1f5a3f8ddb2b1917ed45157dbf9f38
gautam <malaviyagutam0942@gmail.com>
2025-07-09 13:14:39 +0530

    initialize Vite project with React and Tailwind CSS setup

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import HeadTail from './pages/HeadTail';
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/head-tail" element={<HeadTail />} />
+    </Routes>
+    </>
+  )
+}
+
+export default App
